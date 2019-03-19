bdde9eaa385cd235b953b852fd38e318298ce482
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 19:57:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,11 +47,20 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season , holiday_supplies|
+    puts""
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_array = []
+    holiday_hash.each do |season, holiday_supplies_hash|
+      holiday_supplies_hash.each do |holiday, supplies|
+        if supplies.include?("BBQ")
+          bbq_array << holiday
+        end
+      end
+    end
+  bbq_array
 end
