23b788198f91118c193106a90e3bcdc05d3d7537
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 19:58:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  #{new_holiday}: #{supplies.join(", ")}"
     end
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
