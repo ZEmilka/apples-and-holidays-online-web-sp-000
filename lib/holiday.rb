4069deb3425ddc18bb8d3a54ef45fb4c7ca555ce
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 19:07:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-
+   holiday_hash.each do |key , value|
+     key << "supply"
 end
 
 
